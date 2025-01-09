071ea9c9b0f04262b1286d718a120e1e3a8108f8
victorcosta <victorhenriquecosta23@gmail.com>
2025-01-09 16:39:34 -0300

    FIx/foram concluidas todas as paginas que era necessario para o projeto

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :list
-  validates :name, presence: true
+  validates  :name, presence: true
+  validates  :description, presence: true, length: { minimum: 5 }
 
   # Adiciona funcionalidade para ordenar os itens
   acts_as_list scope: :list
