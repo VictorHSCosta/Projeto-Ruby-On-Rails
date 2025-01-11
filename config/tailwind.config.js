c89bec1677708099dffedff02b9d24a6b60b6a3d
victorcosta <victorhenriquecosta23@gmail.com>
2025-01-11 01:46:47 -0300

    Foi migrado toda a parte temporaria do css para o tailwind css algo que deixou o site mais robusto

---
+++ config/tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#5a8f7b",
+        secondary: "#f2b880",
+        background: "#faf3dd",
+        text: "#2a363b",
+        card: "#ffffff",
+      },
+    },
+  },
+  plugins: [
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/container-queries'),
+  ],
+};
